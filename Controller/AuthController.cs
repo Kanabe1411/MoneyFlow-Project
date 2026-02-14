c6725999796684d528984dcc701131563bd11afd
hp <hp@LAPTOP-DFAKVT83>
2026-02-14 15:31:03 +0700

    Add project files.

---
+++ Controller/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyFlow.DTOs;
+using MoneyFlow.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace MoneyFlow.Controller;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    private readonly AuthService _auth;
+
+    public AuthController(AppDbContext context, AuthService auth)
+    {
+        _context = context;
+        _auth = auth;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterDto dto)
+    {
+        if (_context.Users.Any(x => x.Email == dto.Email))
+            return BadRequest("Email exists");
+
+        var user = new User
+        {
+            Email = dto.Email,
+            PasswordHash = _auth.HashPassword(dto.Password)
+        };
+
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        return Ok("Registered");
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginDto dto)
+    {
+        var user = _context.Users.FirstOrDefault(x => x.Email == dto.Email);
+        if (user == null) return Unauthorized();
+
+        if (!_auth.Verify(dto.Password, user.PasswordHash))
+            return Unauthorized();
+
+        return Ok("Login success");
+    }
+}
---
+++ Services/AuthService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Services
+{
+    public class AuthService
+    {
+        public string HashPassword(string password)
+            => BCrypt.Net.BCrypt.HashPassword(password);
+
+        public bool Verify(string password, string hash)
+            => BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
